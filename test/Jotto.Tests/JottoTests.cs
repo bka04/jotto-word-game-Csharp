6eb9bce97dbd33e1b1aca3af7d75de62b5c545f8
Brent Aronsen <bkaronsen@gmail.com>
2021-10-15 16:44:00 -0500

    Test computer playing itself

--- test/Jotto.Tests/JottoTests.cs
+++ test/Jotto.Tests/JottoTests.cs
@@ -18,7 +18,6 @@ namespace Jotto.Tests
             while (randomWord != firstWord && counter < 30) {
                 randomWord = wordList.RandomWord;
                 counter += 1;
-                Console.WriteLine(randomWord);
             }
 
             //Assert
@@ -38,11 +37,50 @@ namespace Jotto.Tests
             while (randomWord != lastWord && counter < 30) {
                 randomWord = wordList.RandomWord;
                 counter += 1;
-                Console.WriteLine(randomWord);
             }
 
             //Assert
             Assert.Equal(lastWord, randomWord);
+        }
+
+        [Fact]
+        public void WordListChecksWord()
+        {
+            //Arrange
+            var wordList = new WordList("Five Letter Words Test");
+            var wordToCheck = "abets";
+            var wordToCheck2 = "zzzzz";
+
+            //Act
+            var wordFound = wordList.CheckWord(wordToCheck);
+            var wordFound2 = wordList.CheckWord(wordToCheck2);
+
+            //Assert
+            Assert.True(wordFound);
+            Assert.False(wordFound2);
+        }
+
+        [Fact]
+        public void ComputerCanSolveJotto()
+        {
+            //Arrange
+            var wordList = new WordList("Five Letter Words Test Full");
+            var wordToGuess = wordList.RandomWord;
+            var guess = "";
+            var counter = 0; //to avoid infinite loop
+            Console.WriteLine($"Word to guess: {wordToGuess}");
+
+            //Act
+            while (guess != wordToGuess && counter < 50) {
+                guess = wordList.RandomWord;
+                var lettersMatched = WordList.GetNumberOfMatchedLetters(guess, wordToGuess);
+                Console.WriteLine($"Guess: {guess}. Letters matched: {lettersMatched}.");
+                wordList.NarrowWordList(guess, lettersMatched);
+                counter++;
+            }
+
+            //Assert
+            Assert.Equal(guess, wordToGuess);
         }
     }
 }
